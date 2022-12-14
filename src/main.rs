cf61133aa5ea421fe8b4834144c9abe07035e3e2
Kavi Laud <kavinath@gmail.com>
2022-12-14 00:10:23 -0600

    Day 9

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use itertools::FoldWhile::{Continue, Done};
-use itertools::Itertools;
 use rstest::*;
 use std::cell::RefCell;
+use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::fmt::Debug;
 #[cfg(test)]
@@ -37,6 +36,8 @@ fn main() {
     println!("{}", day_07_2("./input07.txt"));
     println!("{}", day_08_1("./input08.txt"));
     println!("{}", day_08_2("./input08.txt"));
+    println!("{}", day_09_1("./input09.txt"));
+    println!("{}", day_09_2("./input09.txt"));
 }
 
 struct CaloriesInput {
@@ -936,4 +937,143 @@ fn day_08_2(filename: &str) -> u32 {
 #[test]
 fn test_day_08_2() {
     assert_eq!(day_08_2("./test08.txt"), 8);
+}
+
+type Coord = (i32, i32);
+
+fn move_rope<F>(rope: &mut Vec<Coord>, head_move: Coord, mut after_step: F)
+where
+    F: FnMut(&Vec<Coord>) -> (),
+{
+    assert!(head_move.0 == 0 || head_move.1 == 0);
+    let head_steps = (head_move.0 + head_move.1).abs();
+    for _ in 0..head_steps {
+        rope[0] = (
+            rope[0].0 + head_move.0 / head_steps,
+            rope[0].1 + head_move.1 / head_steps,
+        );
+        for i in 1..rope.len() {
+            let knot = rope[i - 1];
+            let following = rope[i];
+            let distance = ((knot.0 - following.0).pow(2) as f32
+                + (knot.1 - following.1).pow(2) as f32)
+                .sqrt();
+            if distance < 1.5 {
+                continue;
+            } else {
+                let x_incr = match following.0.cmp(&knot.0) {
+                    Ordering::Less => 1,
+                    Ordering::Equal => 0,
+                    Ordering::Greater => -1,
+                };
+                let y_incr = match following.1.cmp(&knot.1) {
+                    Ordering::Less => 1,
+                    Ordering::Equal => 0,
+                    Ordering::Greater => -1,
+                };
+                rope[i] = (following.0 + x_incr, following.1 + y_incr);
+            }
+        }
+        after_step(rope);
+    }
+}
+
+#[rstest]
+#[case::on_top_2(vec![(4, 5), (5, 5)], (1, 0), vec![(5, 5), (5, 5)])]
+#[case::one_left_2(vec![(5, 5), (5, 5)], (-1, 0), vec![(4, 5), (5, 5)])]
+#[case::one_right_2(vec![(5, 5), (5, 5)], (1, 0), vec![(6, 5), (5, 5)])]
+#[case::one_up_2(vec![(5, 5), (5, 5)], (0, 1), vec![(5, 6), (5, 5)])]
+#[case::one_down_2(vec![(5, 5), (5, 5)], (0, -1), vec![(5, 4), (5, 5)])]
+#[case::ne_2(vec![(5, 6), (5, 5)], (1, 0), vec![(6, 6), (5, 5)])]
+#[case::se_2(vec![(4, 4), (5, 5)], (2, 0), vec![(6, 4), (5, 5)])]
+#[case::sw_2(vec![(5, 4), (5, 5)], (-1, 0), vec![(4, 4), (5, 5)])]
+#[case::nw_2(vec![(5, 6), (5, 5)], (-1, 0), vec![(4, 6), (5, 5)])]
+#[case::nne_2(vec![(6, 6), (5, 5)], (0, 1), vec![(6, 7), (6, 6)])]
+#[case::sse_2(vec![(6, 6), (5, 5)], (0, -3), vec![(6, 3), (6, 4)])]
+#[case::ssw_2(vec![(4, 5), (5, 5)], (0, -2), vec![(4, 3), (4, 4)])]
+#[case::nnw_2(vec![(4, 4), (5, 5)], (0, 3), vec![(4, 7), (4, 6)])]
+#[case::ene_2(vec![(5, 6), (5, 5)], (2, 0), vec![(7, 6), (6, 6)])]
+#[case::ese_2(vec![(5, 4), (5, 5)], (2, 0), vec![(7, 4), (6, 4)])]
+#[case::wsw_2(vec![(3, 3), (4, 4)], (0, 1), vec![(3, 4), (4, 4)])]
+#[case::wnw_2(vec![(5, 6), (5, 5)], (-2, 0), vec![(3, 6), (4, 6)])]
+#[case::line_3(vec![(-1, 0), (-1, 0), (-1, 0)], (6, 0), vec![(5, 0), (4, 0), (3, 0)])]
+fn test_move_rope(#[case] mut rope: Vec<Coord>, #[case] m: Coord, #[case] result: Vec<Coord>) {
+    move_rope(&mut rope, m, |_| {});
+    assert_eq!(rope, result);
+}
+
+#[test]
+fn test_move_rope_callback() {
+    let mut rope = vec![(0, 0), (0, 0)];
+    let mut call_count = 0;
+    move_rope(&mut rope, (5, 0), |_| {
+        call_count += 1;
+    });
+    assert_eq!(call_count, 5);
+}
+
+fn moves_from_file(filename: &str) -> Vec<Coord> {
+    read_lines(filename)
+        .unwrap()
+        .into_iter()
+        .filter_map(|r| r.ok())
+        .map(|line| {
+            let (dir, step) = line.split_once(' ').unwrap();
+            match dir {
+                "R" => (step.parse::<i32>().unwrap(), 0),
+                "L" => (-step.parse::<i32>().unwrap(), 0),
+                "U" => (0, step.parse::<i32>().unwrap()),
+                "D" => (0, -step.parse::<i32>().unwrap()),
+                _ => panic!("invalid move direction"),
+            }
+        })
+        .collect()
+}
+
+#[test]
+fn test_moves_from_file() {
+    assert_eq!(
+        moves_from_file("./test09.txt"),
+        [
+            (4, 0),
+            (0, 4),
+            (-3, 0),
+            (0, -1),
+            (4, 0),
+            (0, -1),
+            (-5, 0),
+            (2, 0),
+        ]
+    );
+}
+
+fn day_09(filename: &str, mut rope: Vec<Coord>) -> usize {
+    let mut tail_visits: HashSet<Coord> = HashSet::new();
+    let last_knot_idx = rope.len() - 1;
+    for m in moves_from_file(filename) {
+        move_rope(&mut rope, m, |rope| {
+            tail_visits.insert(rope[last_knot_idx]);
+        })
+    }
+    tail_visits.len()
+}
+
+fn day_09_1(filename: &str) -> usize {
+    day_09(filename, vec![(0, 0), (0, 0)])
+}
+
+fn day_09_2(filename: &str) -> usize {
+    day_09(filename, vec![(0, 0); 10])
+}
+
+#[test]
+fn test_day_09_1() {
+    assert_eq!(day_09_1("./test09.txt"), 13);
+}
+
+#[rstest]
+#[case("./test09.txt", 1)]
+#[case("./test09-2.txt", 36)]
+fn test_day_09_2(#[case] filename: &str, #[case] result: usize) {
+    assert_eq!(day_09_2(filename), result);
 }
